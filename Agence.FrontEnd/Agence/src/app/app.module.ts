c873096312467218e95fbeaacf779022bc2eab5f
Jose G. Moya U <jose.moya@hexasystems.com>
2019-02-14 23:29:56 -0400

    feat: datatable works fine, without multiselect.

--- Agence.FrontEnd/Agence/src/app/app.module.ts
+++ Agence.FrontEnd/Agence/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +15,11 @@ import { MatTableModule,
    MatIconModule,
    MatListModule,
    MatCardModule,
-   MatGridListModule} from '@angular/material';
+   MatGridListModule,
+   MatPaginatorModule,
+   MatSortModule,
+   MatInputModule,
+   MatFormFieldModule} from '@angular/material';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ConsultorListComponent } from './components/consultor-list/consultor-list.component';
 
@@ -38,7 +43,13 @@ import { ConsultorListComponent } from './components/consultor-list/consultor-li
     MatCardModule,
     NgbModule,
     MatGridListModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Agence.FrontEnd/Agence/src/app/components/consultor-list/consultor-list.component.ts
+++ Agence.FrontEnd/Agence/src/app/components/consultor-list/consultor-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -14,13 +14,32 @@ export class ConsultorListComponent implements OnInit {
 
   listData: MatTableDataSource<any>;
   displayedColumns: string[] = ['coUser', 'noUser'];
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  searchKey: string;
 
   constructor(private consultorService: ConsultorService) { }
 
   ngOnInit() {
-     this.consultorService.getConsultors().subscribe( ConsultorResponse => {
+    this.consultorService.getConsultors().subscribe(ConsultorResponse => {
       this.listData = new MatTableDataSource(ConsultorResponse.consultorDto.consultors);
+      this.listData.sort = this.sort;
+      this.listData.paginator = this.paginator;
+      this.listData.filterPredicate = (data, filter) => {
+        return this.displayedColumns.some(ele => {
+          return data[ele].toLowerCase().indexOf(filter) !== -1;
+        });
+      };
     });
+  }
+
+  onSearchClear() {
+    this.searchKey = "";
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    this.listData.filter = this.searchKey.trim().toLowerCase();
   }
 
 }
